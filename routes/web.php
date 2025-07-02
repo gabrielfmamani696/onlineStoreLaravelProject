55aabb5d82578f0b0ad48552fed1ba0dc99fc82f
gab.ait <gabrielfmamanicatari@gmail.com>
2025-07-02 12:09:18 -0400

    Chapter 09 – List Products with Dummy Data, completado

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name("home.index");
 Route::get('/about', [HomeController::class, 'about'])->name("home.about");
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
